513afc7a4127640c02519e8dfd8852cf90dc8b56
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-11-14 22:01:28 -0500

    added clear question and click event to capture answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var choiceA = document.querySelector(".c-1");
 var choiceB = document.querySelector(".c-2");
 var choiceC = document.querySelector(".c-3");
 var choiceD = document.querySelector(".c-4");
+var questionCard = document.querySelector(".question-card");
 
 var timeLeft = 60;
 
@@ -25,7 +26,25 @@ function promptQuestion() {
     choiceB.textContent = "B: Spring";
     choiceC.textContent = "C: Winter";
     choiceD.textContent = "D: Fall";
+    AddChoice();
+}
 
+function AddChoice() {
+    questionCard.addEventListener("click", function(event) {
+        var element = event.target;
+        if(element.matches("li")) {
+            element.setAttribute("style", "color: blue;");
+        }
+    })
+}
+
+
+function clearQuestion() {
+    question.textContent = "";
+    choiceA.textContent = "A: ";
+    choiceB.textContent = "B: ";
+    choiceC.textContent = "C: ";
+    choiceD.textContent = "D: ";
 }
 
 startButton.addEventListener("click", startTime);
