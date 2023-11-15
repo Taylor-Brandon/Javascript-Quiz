c0a5c7a433108af6a217d5456bd9eba7d3030e57
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-11-14 21:38:55 -0500

    added first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 var startButton = document.querySelector(".start-button");
 var timeSection = document.querySelector(".time-left");
+var question = document.querySelector("#question");
+var choiceA = document.querySelector(".c-1");
+var choiceB = document.querySelector(".c-2");
+var choiceC = document.querySelector(".c-3");
+var choiceD = document.querySelector(".c-4");
 
 var timeLeft = 60;
 
@@ -10,7 +15,17 @@ function startTime() {
         if(timeLeft === 0) {
             clearInterval(timerInterval);
         }
-    }, 1000)
+    }, 1000);
+    promptQuestion();
+}
+
+function promptQuestion() {
+    question.textContent = "What is your favorite season?";
+    choiceA.textContent = "A: Summer";
+    choiceB.textContent = "B: Spring";
+    choiceC.textContent = "C: Winter";
+    choiceD.textContent = "D: Fall";
+
 }
 
 startButton.addEventListener("click", startTime);
